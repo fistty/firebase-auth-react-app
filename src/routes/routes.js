eea4cda7896b3ad0308d178213a3d1cf72d5467f
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-02-16 15:31:11 +0100

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 export const SIGN_UP = "/signup";
-export const SIGN_IN = "/signin";
+export const SIGN_IN = "/";
 export const PAGE_NOT_FOUND = "*";
-export const DASHBOARD = "/";
+export const DASHBOARD = "/dashboard";
 export const UPDATE_EMAIL = "/update-email";
 export const UPDATE_PASSWORD = "/update-password";
 export const FORGOT_PASSWORD = "/forgot-password";
