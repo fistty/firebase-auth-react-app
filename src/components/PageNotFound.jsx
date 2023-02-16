8823d64a257e59664d9fd26406c3dd57de5a2812
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-02-16 15:49:18 +0100

    Update PageNotFound.jsx

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate, Link } from "react-router-dom";
 import * as ROUTES from "../routes/routes";
+import "./PageNotFound.css";
 
 export const PageNotFound = () => {
   const navigate = useNavigate();
   return (
-    <div>
+    <div className=".not-found">
       <h1>PAGE DOES NOT EXIST</h1>
-      <Link to={ROUTES.DASHBOARD}></Link>
+      <Link to={ROUTES.DASHBOARD} className="link"></Link>
     </div>
   );
 };
