66014eaaf29d700790cdd93b23621a17e23601d5
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-02-16 15:56:45 +0100

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useUserAuth } from "../context/userAuthContext";
+import { SIGN_IN } from "../routes/routes";
 
 export const ProtectedRoute = ({ children }) => {
   const { currentUser } = useUserAuth();
 
   if (!currentUser) {
-    return <Navigate to="/signin" replace />;
+    return <Navigate to={SIGN_IN} replace />;
   }
 
   return children;
