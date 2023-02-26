95db901294a860e0fff19c73b7492f28bf9907e3
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-02-26 15:37:04 +0100

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBT6svFOzfosN2p8ljloyjSAFHJW91c0Xk",
-  authDomain: "firestore-practice-c15fd.firebaseapp.com",
-  projectId: "firestore-practice-c15fd",
-  storageBucket: "firestore-practice-c15fd.appspot.com",
-  messagingSenderId: "128534099808",
-  appId: "1:128534099808:web:b51f9ab90e8b254ef45005",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
